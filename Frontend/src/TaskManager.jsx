ba56deddc27b91f56e92c1cd8f1d3d78bedf2425
Rushil <rushiltailor9@gmail.com>
2026-07-29 12:40:17 +0530

    final

--- Frontend/src/TaskManager.jsx
+++ Frontend/src/TaskManager.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import { useCallback, useEffect, useState } from 'react';
 import { FaPlus, FaSearch } from 'react-icons/fa'
 import { FaCheck } from "react-icons/fa6";
